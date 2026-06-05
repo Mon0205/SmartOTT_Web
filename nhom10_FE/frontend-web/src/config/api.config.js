374c91330035517154fafcfce414c070be3b552e
Mon0205 <nhuy02042003@gmail.com>
2026-06-05 10:48:39 +0700

    commit

--- nhom10_FE/frontend-web/src/config/api.config.js
+++ nhom10_FE/frontend-web/src/config/api.config.js
@@ -1,5 +1,5 @@
-const DEFAULT_BACKEND_URL = "http://localhost:3000";
-const DEFAULT_SOCKET_URL = DEFAULT_BACKEND_URL;
+const DEFAULT_BACKEND_URL = "https://smartott-web.onrender.com";
+const DEFAULT_SOCKET_URL = "https://smartott-web.onrender.com";
 
 export const BACKEND_URL = (
   import.meta.env.VITE_BACKEND_URL || DEFAULT_BACKEND_URL
